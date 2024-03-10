a4d4a37d06eabc318572bf96db902ad23350c700
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-09 23:56:48 -0500

    updated viewpoints

--- create_viewpoints.py
+++ create_viewpoints.py
@@ -40,37 +40,40 @@
 
 import math
 
+def calculate_face_center(vertices):
+    # Calculate the average of the vertices to find the center
+    return [sum(coord) / len(vertices) for coord in zip(*vertices)]
+
 if __name__ == '__main__':
     circumradius = math.sqrt(3)
     distance = circumradius * 1.2
     # Define the vertices of a cube
-    cube = [[-1, -1, -1],
-            [1, -1, -1],
-            [1, 1, -1],
-            [-1, 1, -1],
-            [-1, -1, 1],
-            [1, -1, 1],
-            [1, 1, 1],
-            [-1, 1, 1]]
-    
-    # Define the indices of vertices that form each face of the cube
-    # cube_faces = [
-    #     [0, 1, 2, 3],  # Bottom face
-    #     [4, 5, 6, 7],  # Top face
-    #     [0, 1, 5, 4],  # Side face 1
-    #     [1, 2, 6, 5],  # Side face 2
-    #     [2, 3, 7, 6],  # Side face 3
-    #     [3, 0, 4, 7]   # Side face 4
-    # ]
+    vertices = [
+        [-1, -1, -1],
+        [1, -1, -1],
+        [1, 1, -1],
+        [-1, 1, -1],
+        [-1, -1, 1],
+        [1, -1, 1],
+        [1, 1, 1],
+        [-1, 1, 1]
+    ]
 
-    # Get Azimuth, Elevation angles
-    # Azimuth varies from -pi to pi
-    # Elevation from -pi/2 to pi/2
-    view_points = open('./view_points.txt', 'w+')
-    for face in cube:
-        # Calculate the center of the face
-        center = [sum([cube[i][j] for i in face]) / len(face) for j in range(3)]
-        elevation = math.asin(center[2] / circumradius)
-        azimuth = math.atan2(center[1], center[0])
-        view_points.write('%f %f %f %f\n' % (azimuth, elevation, 0., distance))
-    view_points.close()
+    # Define faces using indices from the vertices list
+    faces = [
+        [0, 1, 2, 3], # Bottom face
+        [4, 5, 6, 7], # Top face
+        [0, 1, 5, 4], # Side face 1
+        [2, 3, 7, 6], # Side face 2
+        [0, 3, 7, 4], # Side face 3
+        [1, 2, 6, 5]  # Side face 4
+    ]
+
+    # Open a file to write the view points
+    with open('./view_points.txt', 'w+') as view_points:
+        for face_indices in faces:
+            face_vertices = [vertices[i] for i in face_indices]
+            center = calculate_face_center(face_vertices)
+            elevation = math.asin(center[2] / circumradius)
+            azimuth = math.atan2(center[1], center[0])
+            view_points.write(f'{azimuth} {elevation} 0.0 {distance}\n')

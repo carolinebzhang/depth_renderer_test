092737a4977b04d924acb0af7c09b0f3b7801a73
carolinebzhang <caroline_b_zhang@brown.edu>
2024-03-16 21:37:18 -0400

    skjdhf

--- reorganize.sh
+++ reorganize.sh
@@ -12,15 +12,21 @@ mkdir -p "$dest_dir"
 # Function to copy images to distinct folders
 copy_images() {
     local source_folder="$1"
-    local dest_folder="$dest_dir/$2"
-    mkdir -p "$dest_folder"
+    #local dest_folder="$dest_dir/$num"
+    mkdir -p "$dest_folder/001"
+    mkdir -p "$dest_folder/002"
+    mkdir -p "$dest_folder/003"
+    mkdir -p "$dest_folder/004"
+    mkdir -p "$dest_folder/005"
+    mkdir -p "$dest_folder/006"
     for image in "$source_folder"/*.png; do
         if [[ -f "$image" ]]; then
             # Extract the number part of the filename
             num=$(basename "$image" | grep -oE '[0-9]+')
+            
 
             # Copy the image to the corresponding destination folder
-            cp "$image" "$dest_folder"
+            cp "$image" "$dest_folder/$num"
         fi
     done
 }
